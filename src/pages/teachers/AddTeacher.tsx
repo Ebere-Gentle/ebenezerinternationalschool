a74f52f7a0386b49173c8e2fd270f89dca44ef2b
Ebere-Gentle <eberegentlevictor@gmail.com>
2026-09-05 23:28:52 +0100

    Your message

--- src/pages/teachers/AddTeacher.tsx
+++ src/pages/teachers/AddTeacher.tsx
@@ -25,7 +25,7 @@ import {
   Users,
   X,
 } from 'lucide-react';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../../config/supabase/client';
 
 /* =========================================================
    TYPES
