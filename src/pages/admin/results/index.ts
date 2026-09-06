96b12988d56b00e6d8fefa056db25d664d219758
codewithGEneration <eberegentlevictor@gmail.com>
2026-09-06 09:27:32 +0100

    fix: restore admin results barrel export

---
+++ src/pages/admin/results/index.ts
@@ -0,0 +1,5 @@
+export { default as AdminEnterTest } from './AdminEnterTest';
+export { default as AdminEnterExam } from './AdminEnterExam';
+export { default as AdminEnterCBT } from './AdminEnterCBT';
+export { default as AdminViewResults } from './AdminViewResults';
+export { default as AdminResultSummary } from './AdminResultSummary';
