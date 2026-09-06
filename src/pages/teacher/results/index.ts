c8c5e7e9f0841fb31832490da8178b4b22440c3f
codewithGEneration <eberegentlevictor@gmail.com>
2026-09-06 09:27:35 +0100

    fix: restore teacher results barrel export

---
+++ src/pages/teacher/results/index.ts
@@ -0,0 +1,5 @@
+export { default as TeacherEnterTest } from './TeacherEnterTest';
+export { default as TeacherEnterExam } from './TeacherEnterExam';
+export { default as TeacherEnterCBT } from './TeacherEnterCBT';
+export { default as TeacherViewResults } from './TeacherViewResults';
+export { default as TeacherResultSummary } from './TeacherResultSummary';
