2991106b47357c51652ca280fd31035f554f9a89
codewithGEneration <eberegentlevictor@gmail.com>
2026-09-06 09:27:38 +0100

    fix: restore student results barrel export

---
+++ src/pages/student/results/index.ts
@@ -0,0 +1,4 @@
+export { default as StudentViewTest } from './StudentViewTest';
+export { default as StudentViewExam } from './StudentViewExam';
+export { default as StudentViewCBT } from './StudentViewCBT';
+export { default as StudentResultSummary } from './StudentResultSummary';
