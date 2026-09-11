302fb376ccd6eaf8cbb21943c7e379c8bc6355a2
codewithGEneration <eberegentlevictor@gmail.com>
2026-09-12 00:58:50 +0100

    feat: add teacher second test entry

---
+++ src/pages/teacher/results/TeacherEnterSecondTest.tsx
@@ -0,0 +1,5 @@
+import TeacherResultEntry from './TeacherResultEntry';
+
+const TeacherEnterSecondTest = () => <TeacherResultEntry assessmentType="second_test" />;
+
+export default TeacherEnterSecondTest;
