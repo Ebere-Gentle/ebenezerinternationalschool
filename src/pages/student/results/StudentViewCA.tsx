36f71ebf87c006074dae1457db4cf6e59305074e
codewithGEneration <eberegentlevictor@gmail.com>
2026-09-12 04:22:20 +0100

    feat: add student CA results page

---
+++ src/pages/student/results/StudentViewCA.tsx
@@ -0,0 +1,5 @@
+import { PublishedResults } from '../../../components/results/shared/PublishedResults';
+
+export default function StudentViewCA() {
+  return <PublishedResults assessmentType="ca" audience="student" />;
+}
