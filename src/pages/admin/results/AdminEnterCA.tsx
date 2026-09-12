8978e28e74287fef24bf188f26d295e8b3e8a8be
codewithGEneration <eberegentlevictor@gmail.com>
2026-09-12 03:03:23 +0100

    feat: add tabs to CA result entry

--- src/pages/admin/results/AdminEnterCA.tsx
+++ src/pages/admin/results/AdminEnterCA.tsx
@@ -1,5 +1,10 @@
 import AdminResultEntry from './AdminResultEntry';
+import AdminResultAssessmentTabs from './AdminResultAssessmentTabs';
 
-const AdminEnterCA = () => <AdminResultEntry assessmentType="ca" />;
+const AdminEnterCA = () => (
+  <AdminResultAssessmentTabs>
+    <AdminResultEntry assessmentType="ca" />
+  </AdminResultAssessmentTabs>
+);
 
 export default AdminEnterCA;
