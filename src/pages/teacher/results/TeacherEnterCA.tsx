43c62596fdd06457f7a0c2c27695f16fa058422f
codewithGEneration <eberegentlevictor@gmail.com>
2026-09-12 03:03:54 +0100

    feat: add tabs to teacher CA result entry

--- src/pages/teacher/results/TeacherEnterCA.tsx
+++ src/pages/teacher/results/TeacherEnterCA.tsx
@@ -1,5 +1,10 @@
 import TeacherResultEntry from './TeacherResultEntry';
+import TeacherResultAssessmentTabs from './TeacherResultAssessmentTabs';
 
-const TeacherEnterCA = () => <TeacherResultEntry assessmentType="ca" />;
+const TeacherEnterCA = () => (
+  <TeacherResultAssessmentTabs>
+    <TeacherResultEntry assessmentType="ca" />
+  </TeacherResultAssessmentTabs>
+);
 
 export default TeacherEnterCA;
