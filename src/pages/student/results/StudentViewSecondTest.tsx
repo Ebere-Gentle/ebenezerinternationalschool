9b287bb7fd8720267682308a2395bccce8a3a3bf
codewithGEneration <eberegentlevictor@gmail.com>
2026-09-12 04:22:13 +0100

    feat: add student Test 2 results page

---
+++ src/pages/student/results/StudentViewSecondTest.tsx
@@ -0,0 +1,5 @@
+import { PublishedResults } from '../../../components/results/shared/PublishedResults';
+
+export default function StudentViewSecondTest() {
+  return <PublishedResults assessmentType="second_test" audience="student" />;
+}
